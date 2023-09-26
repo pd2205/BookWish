f268d5c623d034eacc805745ffe0a5c3186049bc
pd2205 <pridhi2205@gmail.com>
2023-09-26 20:17:18 +0530

    update crud functions to sync with api

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,56 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import BookCreate from './components/BookCreate'
 import BookList from './components/BookList'
+import axios from 'axios'
 
 const App = () => {
 
   const [books, setBooks] = useState([])
 
-  const createBook = (title) => {
-    const newBook = {id: Math.round(Math.random() * 9999), title: title}
-    console.log(newBook);
-    const updatedBooks = [...books, newBook]
-    setBooks(updatedBooks)
-    console.log(books)
+  const fetchBooks = async () => {
+    // Making a GET req to fetch books
+
+    const response = await axios.get("http://localhost:3001/books")
+    setBooks(response.data)
   }
 
-  const deleteBookById = (id) => {
+  useEffect(() => {
+    fetchBooks()
+  }, [])
+
+  const createBook = async (title) => {
+    // Making a POST Req to create a book
+
+    const response = await axios.post("http://localhost:3001/books", {
+      title: title
+    })
+
+    const newBook = {id: response.data.id, title: response.data.title}
+    const updatedBooks = [...books, newBook]
+    setBooks(updatedBooks)
+  }
+
+  const deleteBookById = async (id) => {
+    // Making a DELETE req to delete a book by its id
+
+    await axios.delete(`http://localhost:3001/books/${id}`)
+
     const updatedBooks = books.filter((book) => {
       return id !== book.id
     })
     setBooks(updatedBooks)
   }
 
-  const editBookById = (id, title) => {
+  const editBookById = async (id, title) => {
+    // Making a PUT req to update a title of book by its id 
+
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: title
+    })
+
     const updatedBooks = books.map((book) => {
-      if(id === book.id) {
-        return {...book, title: title}
+      if(response.data.id === book.id) {
+        return response.data
       }
       return book
     })
